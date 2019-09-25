402f6d1b8c4184286e51759312249db2b8a05196
dinesh693 <38497237+dinesh693@users.noreply.github.com>
2019-09-25 22:16:14 +1000

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,5 +1,6 @@
-﻿Add-Type -AssemblyName PresentationFramework
-Import-Module GUI.ps1
+Add-Type -AssemblyName PresentationFramework
+
+Import-Module .\PSTools\GUI.ps1
 #############################################################################################
 
 
@@ -56,23 +57,36 @@ $get_process.Add_Click({
     }
     else{
     $header1.Header = "Process Name"
-    $header2.Header = "ID"
     $header3.Header = "Memory"
-    $process = Get-Process -ComputerName $cinumber.Text | Sort-Object Name
-    $Bindable_Process = $process | Select-Object -Property @{Name='Name';Expression={$_.ProcessName}}, @{Name='ID';Expression={$_.ID}}, @{Name='PM';Expression={$_.PM}}
+    $process = Get-Process <# -ComputerName $cinumber.Text -ErrorVariable get_process_error #>| Sort-Object Name
+    $Bindable_Process = $process | Select-Object -Property @{Name='Name';Expression={$_.ProcessName}}, @{Name='PM';Expression={$_.PM}}
     $listview_process.ItemsSource = $Process
+    if($get_process_error){ $ping_result_show.Text = $get_process_error} #"Unable to retrieve process from " +  $cinumber.Text + ". Check PC is online or is not local PC"}
     }
+    
 })
 ################################ Get Process Event ends
 ################################ End Process Event starts
 $end_task.Add_Click({
-    $process_name = $listview_process.SelectedItem.ID
-    Write-Host $process_name
-    $kill_task = taskkill /F /IM $process_name /T
+    #$process_name = $listview_process.SelectedItem.Name
+    $process_name = Get-WmiObject -Class Win32_Process -ComputerName $ComputerName -Filter "name='$listview_process.SelectedItem.Name'"
+    foreach($kill_process in $process_name){
+        $result = $kill_process.terminate()
+        $processid = $kill_process.handle
+        
+        if($result.returnvalue -eq 0){
+            $ping_result_show.Text = "The process $kill_process terminated succesfully."
+        }
+        else{
+            $ping_result_show.Text = "The process $kill_process termination has some problems."
+        }  
+    }
+    #$kill_task = taskkill /F /IM $process_name /T
+
     $ping_result_show.Text = $kill_task
     Start-Sleep 3.5
     $process = Get-Process | Sort-Object Name
-    $Bindable_Process = $process | Select-Object -Property @{Name='Name';Expression={$_.ProcessName}}, @{Name='ID';Expression={$_.ID}}, @{Name='PM';Expression={$_.PM}}
+    $Bindable_Process = $process | Select-Object -Property @{Name='Name';Expression={$_.ProcessName}}, @{Name='PM';Expression={$_.PM}}
     $listview_process.ItemsSource = $Process
 
 })
@@ -81,10 +95,9 @@ $end_task.Add_Click({
 
 $get_installed_apps.Add_Click({
     $header1.Header = "Application Name"
-    $header2.Header = "Vendor"
-    $header3.Header = ""
+    $header3.Header = "Vendor"
     $installed_apps = Get-Wmiobject -Class Win32_Product -ComputerName $cinumber.Text | select Name, Vendor | sort Name
-    $Bindable = $installed_apps | Select-Object -Property @{Name='Name';Expression={$_.Name}}, @{Name='ID';Expression={$_.Vendor}}
+    $Bindable = $installed_apps | Select-Object -Property @{Name='Name';Expression={$_.Name}}, @{Name='PM';Expression={$_.Vendor}}
     $listview_process.ItemsSource = $Bindable
 })
 
@@ -104,18 +117,32 @@ $app_uninstall.Add_Click({
 $username_lastlogon = $window.FindName("textBox_username")
 $lastlogon = $window.FindName("button_lastlogon")
 $textbox_result =$window.FindName("textbox_lastlogon")
+$button_memberof =$window.FindName("button_memberof")
+$listview_memberof = $window.FindName("listView_membership")
+$memberof_header1 = $window.FindName("memberof_header1")
+$memberof_header2 = $window.FindName("memberof_header2")
+
 
 ################################ Variable declaration for form items in Tab 2 ends
-
-
-
+##################s####################################### Get User Last logon start
 $lastlogon.Add_Click({
 
-    $result = Get-ADUser -Identity $username_lastlogon.text -Properties LastLogon -server dcpdc03|   Format-Table UserPrincipalName, @{Name='LastLogon';Expression={[DateTime]::FromFileTime($_.LastLogon)}} | fl | out-string;
+    $result = Get-ADUser -Identity $username_lastlogon.text -Properties LastLogon -server dcpdc03|Select-Object @{Name='LastLogon';Expression={[DateTime]::FromFileTime($_.LastLogon)}} | fl | out-string;
     $textbox_result.text = $result
 
 })
+##################s####################################### Get User Last logon Ends
+##################s####################################### Get User Membership start
 
+$button_memberof.Add_Click({
+    $member_of = Get-ADPrincipalGroupMembership -Identity $username_lastlogon.text |Sort-Object -Property  @{Expression={$_.GroupCategory}}, @{Expression={$_.Name}}  | Select-Object  Name, GroupCategory 
+    #$memberof_header1.Header = "ACL Name"
+    #$memberof_header2.Header = "E G"
+    $Bindable_memberof = $member_of | Select-Object -Property @{Name='ACLName';Expression={$_.Name}} , @{Name='GroupCategory';Expression={$_.GroupCategory} }
+    $listview_memberof.ItemsSource = $Bindable_memberof
+})
+
+##################s####################################### Get User Membership Ends
 ########################################################## Tab 2 Active Directory Ends
 ########################################################## Tab 3 Install MIS Starts
 ################################ Variable declaration for form items in Tab 3 starts
