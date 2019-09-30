df5d39aea41b02bf36bef1234edf9dc762669dbd
dinesh693 <38497237+dinesh693@users.noreply.github.com>
2019-09-30 15:55:37 +1000

    Update GUI.ps1

--- GUI.ps1
+++ GUI.ps1
@@ -1,5 +1,4 @@
 [xml]$xaml = @"
-
 <Window
   xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
   xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
@@ -12,13 +11,13 @@
                     <ColumnDefinition Width="188*"/>
                 </Grid.ColumnDefinitions>
                 <Label x:Name="label" Content="Enter PC Name" HorizontalAlignment="Left" Height="31" Margin="10,6,0,0" VerticalAlignment="Top" Width="140"/>
-                <TextBlock x:Name="textBlock1" HorizontalAlignment="Left" Height="177" Margin="13,66,0,0" TextWrapping="WrapWithOverflow" VerticalAlignment="Top" Width="321" Grid.ColumnSpan="2"/>
+                <TextBlock x:Name="textBlock1" HorizontalAlignment="Left" Height="292" Margin="13,66,0,0" TextWrapping="WrapWithOverflow" VerticalAlignment="Top" Width="316" Grid.ColumnSpan="2" Background="WhiteSmoke"/>
                 <TextBox x:Name="textBox_CInumber" HorizontalAlignment="Left" Height="28" Margin="131,9,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="155" Grid.ColumnSpan="2"/>
                 <ListView x:Name="listView_process" HorizontalAlignment="Left" Height="318" Margin="133,71,0,0" VerticalAlignment="Top" Width="423" Grid.Column="1">
                     <ListView.View>
                         <GridView>
                             <GridViewColumn x:Name="header1" Header="" DisplayMemberBinding="{Binding Name}" />
-                            <GridViewColumn x:Name="header3" Header="" DisplayMemberBinding="{Binding PM}" />
+                            <GridViewColumn x:Name="header3" Header="" DisplayMemberBinding="{Binding ID}" />
                         </GridView>
                     </ListView.View>
                 </ListView>
@@ -28,7 +27,11 @@
                     <Button x:Name="button_get_process" Content="Process" HorizontalAlignment="Left" Height="21" VerticalAlignment="Top" Width="135" Canvas.Left="3" Canvas.Top="34"/>
                     <Button x:Name="button_end_task" Content="End Task" HorizontalAlignment="Left" Height="21" VerticalAlignment="Top" Width="137" Canvas.Left="142" Canvas.Top="34"/>
                     <Button x:Name="button_get_installed_apps" Content="Get Installed Apps" HorizontalAlignment="Left" Height="21" VerticalAlignment="Top" Width="142" Canvas.Left="284" Canvas.Top="13"/>
-                    <Button x:Name="button_uninstall" Content="Uninstall Selected" HorizontalAlignment="Left" Height="21" VerticalAlignment="Top" Width="142" Canvas.Left="284" Canvas.Top="34"/>
+                    <Button x:Name="button_pc_details" Content="PC Details" HorizontalAlignment="Left" Height="21" VerticalAlignment="Top" Width="142" Canvas.Left="284" Canvas.Top="34"/>
+                </Canvas>
+                <Canvas Grid.ColumnSpan="2" HorizontalAlignment="Left" Height="73" Margin="13,358,0,0" VerticalAlignment="Top" Width="316" Background="#FFC5BCBC">
+                    <Button x:Name="button_flushdns" Content="Flush DNS" Height="25" Canvas.Left="11" Canvas.Top="5" Width="94"/>
+                    <Button x:Name="button_displaydns" Content="DNS Details" Height="25" Canvas.Left="11" Canvas.Top="32" Width="94"/>
                 </Canvas>
             </Grid>
         </TabItem>
@@ -44,8 +47,7 @@
                     <TextBox x:Name="textBox_username" HorizontalAlignment="Left" Height="23" TextWrapping="Wrap" VerticalAlignment="Top" Width="106" Canvas.Left="83" Canvas.Top="27"/>
                     <Button x:Name="button_lastlogon" Content="Last Logon" HorizontalAlignment="Left" Height="28" VerticalAlignment="Top" Width="81" Canvas.Left="194" Canvas.Top="25"/>
                     <TextBlock x:Name="textbox_lastlogon" HorizontalAlignment="Left" Height="117" TextWrapping="Wrap" VerticalAlignment="Top" Width="265" Canvas.Left="10" Canvas.Top="55"/>
-                    <Button x:Name="button_memberof" Content="Member Of" HorizontalAlignment="Left" Height="28" VerticalAlignment="Top" Width="81" Canvas.Left="283" Canvas.Top="57"/>
-                    <Button x:Name="button_check_folderaccess" Content="Check Folder Access" HorizontalAlignment="Left" Height="28" VerticalAlignment="Top" Width="119" Canvas.Left="283" Canvas.Top="24"/>
+                    <Button x:Name="button_memberof" Content="Member Of" HorizontalAlignment="Left" Height="28" VerticalAlignment="Top" Width="81" Canvas.Left="280" Canvas.Top="25"/>
                 </Canvas>
                 <ListView x:Name="listView_membership" Grid.Column="2" HorizontalAlignment="Left" Height="337" Margin="192,10,0,0" VerticalAlignment="Top" Width="329">
                     <ListView.View>
@@ -62,25 +64,13 @@
                 </Canvas>
                 <TextBox x:Name="textBox" Grid.ColumnSpan="2" Grid.Column="1" HorizontalAlignment="Left" Height="30" Margin="44,212,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="293"/>
                 <Button x:Name="button1" Content="Button" Grid.Column="2" HorizontalAlignment="Left" Margin="852,74,-396,0" VerticalAlignment="Top" Width="75"/>
-            </Grid>
-        </TabItem>
-        <TabItem Header="Install MSI or EXE">
-            <Grid Background="#FFE5E5E5">
-                <Grid.ColumnDefinitions>
-                    <ColumnDefinition Width="5*"/>
-                    <ColumnDefinition Width="73*"/>
-                </Grid.ColumnDefinitions>
-                <Button x:Name="button_openfile" Content="Open FIle ....." Grid.Column="1" HorizontalAlignment="Left" Height="30" Margin="27,23,0,0" VerticalAlignment="Top" Width="109"/>
-                <TextBox x:Name="textBox_openfile" Grid.Column="1" HorizontalAlignment="Left" Height="30" Margin="155,23,0,0" TextWrapping="Wrap" Text="" VerticalAlignment="Top" Width="464"/>
-                <TextBox x:Name="textBox_copyto" Grid.Column="1" HorizontalAlignment="Left" Height="27" Margin="156,65,0,0" TextWrapping="Wrap" Text="" VerticalAlignment="Top" Width="165"/>
-                <Label x:Name="label2" Content="CI Number to Install" Grid.Column="1" HorizontalAlignment="Left" Height="27" Margin="27,67,0,0" VerticalAlignment="Top" Width="117"/>
-                <Button x:Name="button_install" Content="Install" Grid.Column="1" HorizontalAlignment="Left" Height="28" Margin="156,113,0,0" VerticalAlignment="Top" Width="79"/>
-                <Button x:Name="button_install_Copy" Content="Copy" Grid.Column="1" HorizontalAlignment="Left" Height="28" Margin="326,65,0,0" VerticalAlignment="Top" Width="79"/>
+                <Button x:Name="button_remove_memberof" Content="Remove Access" HorizontalAlignment="Left" Height="28" VerticalAlignment="Top" Width="109" Grid.Column="2" Margin="192,352,0,0"/>
+                <Button x:Name="button_remove_distribution" Content="Remove DL Access" HorizontalAlignment="Left" Height="28" VerticalAlignment="Top" Width="109" Grid.Column="2" Margin="192,381,0,0"/>
+                <Button x:Name="button_backup" Content="Backup Access" HorizontalAlignment="Left" Height="28" VerticalAlignment="Top" Width="109" Grid.Column="2" Margin="192,410,0,0"/>
+
             </Grid>
         </TabItem>
 
     </TabControl>
 </Window>
-
 "@
-
